1e5da3f6ce2639dbfefb799670c770ef6649188a
MateuszK <mateusz.konopka11@gmail.com>
2020-07-20 13:35:13 +0200

    Register new users functionality added to SPA

--- Mangio-SPA/src/app/register/register.component.ts
+++ Mangio-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
+
+  registerr() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration success');
+    }, error => {
+      console.log('register failed');
+    });
+  }
+
+
 
 }
