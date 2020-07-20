c4280d192cbef66db11a148aca1a995ba8fcfecd
MateuszK <mateusz.konopka11@gmail.com>
2020-07-20 11:23:19 +0200

    Routing added

--- Mangio-SPA/src/app/app.module.ts
+++ Mangio-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProjectComponent } from './project/project.component';
@@ -9,6 +10,8 @@ import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { appRoutes } from './routes';
 
 @NgModule({
    declarations: [
@@ -16,12 +19,14 @@ import { LoginComponent } from './login/login.component';
       ProjectComponent,
       NavComponent,
       HomeComponent,
-      LoginComponent
+      LoginComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService
