ffcd6e38c00a7a8cec59478cfc196370d478f6b7
MateuszK <mateusz.konopka11@gmail.com>
2020-07-23 14:20:24 +0200

    Navbar update

--- Mangio-SPA/src/app/app.module.ts
+++ Mangio-SPA/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { appRoutes } from './routes';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
    declarations: [
@@ -22,7 +23,8 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
       NavComponent,
       HomeComponent,
       LoginComponent,
-      RegisterComponent
+      RegisterComponent,
+      UsersComponent
    ],
    imports: [
       BrowserModule,
--- Mangio-SPA/src/app/routes.ts
+++ Mangio-SPA/src/app/routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { UsersComponent } from './users/users.component';
 
 export const appRoutes: Routes = [
-    { path: 'register', component: RegisterComponent },
+    { path: 'users/register', component: RegisterComponent },
+    { path: 'users', component: UsersComponent },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
